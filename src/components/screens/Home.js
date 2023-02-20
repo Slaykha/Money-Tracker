4006982a07bf6fb425abd1fedf41aecec0f7b631
Kadir Engin <kadirengin624@hotmail.com>
2023-02-20 10:17:49 +0300

    add first info box to home screen.

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -7,6 +7,19 @@ const useStyles = makeStyles(() =>({
     width:"calc(100% - 250px)",
     height:300,
     position:"absolute",
+  },
+  Box:{
+    background:"#1A1A1A",
+    width:200,
+    height:120,
+    borderRadius:10,
+    color:"#D3D3D3",
+    marginTop:"250px",
+    marginLeft:"3%",
+    paddingLeft:"35px"
+  },
+  boxText: {
+    paddingTop:"20px"
   }
 }))
 
@@ -15,8 +28,12 @@ export const Home = () => {
   return (
     <div>
       <div className={classes.homeHeader}>
-
+        <div className={classes.Box}>
+          <div className={classes.boxText}> Total Spendings </div>
+          <div className={classes.boxText}> ₺5.514,52</div>
+        </div>
       </div>
+      
     </div>
   )
 }
